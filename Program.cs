c47b786e966fe69cbf3a1c533f4c33ba25034647
Satantra Tewari <satantra1508@gmail.com>
2020-10-09 00:12:16 +0530

    [satantra] first name validated

--- Program.cs
+++ Program.cs
@@ -7,6 +7,18 @@ namespace User_Registration_Problem
         static void Main(string[] args)
         {
             Console.WriteLine(" Welcome to User Registration Problem ");
+            User_Registration_Details userRegistrationDetails = new User_Registration_Details();
+        RegStart1:
+            Console.WriteLine(" Enter the first name(starting with capital and minimum 3 letters) ");
+            string firstName = Console.ReadLine();
+            bool checkFirstName = userRegistrationDetails.validateFirstName(firstName);
+            if (checkFirstName == true)
+                Console.WriteLine(" FIRST NAME: " + firstName);
+            else
+            {
+                Console.WriteLine(" Please enter valid first name ");
+                goto RegStart1;
+            }
         }
     }
 }
---
+++ User_Registration_Details.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace User_Registration_Problem
+{
+    class User_Registration_Details
+    {
+        public static string REGEX_NAME = "^[A-Z]{1,}[A-Za-z]{2,}$";
+        public bool validateFirstName(string firstName)
+        {
+            return Regex.IsMatch(firstName, REGEX_NAME);
+        }
+    }
+}
